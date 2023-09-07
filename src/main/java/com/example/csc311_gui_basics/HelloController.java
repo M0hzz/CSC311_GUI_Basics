31298201623dbf1fc16be22e562ea53f50b0f095
Mehul Rajpal <mehulraj925@gmail.com>
2023-09-07 13:42:39 -0400

    Added circle

--- src/main/java/com/example/csc311_gui_basics/HelloController.java
+++ src/main/java/com/example/csc311_gui_basics/HelloController.java
@@ -2,13 +2,19 @@ package com.example.csc311_gui_basics;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onDrag(MouseEvent mouseEvent) {
+
     }
 }
